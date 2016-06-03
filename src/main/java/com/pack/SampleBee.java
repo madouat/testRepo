f53f1d94040f58979791a1698ff2609316f9ce44
Mamadou Ouattara <madouat@gmail.com>
2016-06-03 11:37:31 -0400

    remove useless
    touch pom.xml file
    
    touch
    
    fdsfsadf (rewrote me)
    
    sample jacoco usage

---
+++ src/main/java/com/pack/SampleBee.java
@@ -0,0 +1,10 @@
+package com.pack;
+
+public class SampleBee {
+
+	private String data;
+
+	public String dataBuild(String tit) {
+		return "";
+	}
+}
--- src/main/java/com/pack/TestClass.java
+++ src/main/java/com/pack/TestClass.java
@@ -1,10 +1,10 @@
-package com.test;
+package com.pack;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("toto titi tata");
+		System.out.println("toto titi tata llooo");
 
 	}
 
